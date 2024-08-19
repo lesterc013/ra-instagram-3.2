54ddebc798f22f773604e425363aeff603152a1c
lester <lesterc013@gmail.com>
2024-08-19 17:59:46 +0800

    Basic firebase syntax practice to demonstrate realtime db update and get

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,83 @@
-import logo from "/logo.png";
-import "./App.css";
-import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
-import { useState, useEffect } from "react";
+import logo from '/logo.png'
+import './App.css'
+import { onChildAdded, push, ref, set } from 'firebase/database'
+import { database } from './firebase'
+import { useState, useEffect } from 'react'
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
-const DB_MESSAGES_KEY = "messages";
+const DB_MESSAGES_KEY = 'messages'
 
 function App() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([])
+  const [username, setUsername] = useState('')
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
+    const messagesRef = ref(database, DB_MESSAGES_KEY)
     // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
+    onChildAdded(messagesRef, data => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      setMessages((prevState) =>
+      setMessages(prevState =>
         // Store message key so we can use it as a key in our list items when rendering messages
         [...prevState, { key: data.key, val: data.val() }]
-      );
-    });
-  }, []);
+      )
+    })
+  }, [])
 
   const writeData = () => {
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
-  };
+    const messageListRef = ref(database, DB_MESSAGES_KEY)
+    const newMessageRef = push(messageListRef)
+    set(newMessageRef, {
+      username,
+      message,
+      time: new Date().toLocaleString(),
+    })
+  }
 
-  // Convert messages in state to message JSX elements to render
-  let messageListItems = messages.map((message) => (
-    <li key={message.key}>{message.val}</li>
-  ));
+  // // Convert messages in state to message JSX elements to render
+  // let messageListItems = messages.map(message => (
+  //   <li key={message.key}>{message.val}</li>
+  // ))
 
   return (
     <>
       <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
+        <img src={logo} className='logo' alt='Rocket logo' />
       </div>
       <h1>Instagram Bootcamp</h1>
-      <div className="card">
+      <div className='card'>
         {/* TODO: Add input field and add text input as messages in Firebase */}
-        <button onClick={writeData}>Send</button>
-        <ol>{messageListItems}</ol>
+        <form onSubmit={writeData}>
+          <label>Username</label>
+          <input
+            type='text'
+            name='username'
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <label>Message</label>
+          <input
+            type='text'
+            name='message'
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <button type='submit'>Send</button>
+        </form>
+        <ol>
+          {messages.length === 0
+            ? null
+            : messages.map(messageItem => (
+                <li key={messageItem.key}>
+                  <p>{messageItem.val.username}</p>
+                  <p>Message: {messageItem.val.message}</p>
+                  <p>Time: {messageItem.val.time}</p>
+                </li>
+              ))}
+        </ol>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.REGION.firebasedatabase.app",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-};
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+}
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig)
 
 // Get a reference to the database service and export the reference for other modules
-export const database = getDatabase(firebaseApp);
+export const database = getDatabase(firebaseApp)
