f5feca8cf986e1a994faaa93986920360822a9a4
lester <lesterc013@gmail.com>
2024-08-22 12:30:35 +0800

    Changed Form name to Composer

---
+++ src/Components/Composer.jsx
@@ -0,0 +1,42 @@
+const Composer = ({
+  username,
+  setUsername,
+  message,
+  setMessage,
+  setFile,
+  handleSubmit,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Username</label>
+      <br />
+      <input
+        type='text'
+        name='username'
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+      <br />
+      <label>Message</label>
+      <br />
+      <input
+        type='text'
+        name='message'
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <br />
+      <label>Image</label>
+      <br />
+      <input
+        type='file'
+        name='file'
+        onChange={e => setFile(e.target.files[0])}
+      />
+      <br />
+      <button type='submit'>Post!</button>
+    </form>
+  )
+}
+
+export default Composer
